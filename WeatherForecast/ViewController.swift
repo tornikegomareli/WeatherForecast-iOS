330689863413fd704dc410603d4154a6eea9f190
TornikeGomareli <gomareli@itstep.org>
2019-05-28 12:59:54 +0400

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Tornike Gomareli on 5/28/19.
+//  Copyright © 2019 Tornike Gomareli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
